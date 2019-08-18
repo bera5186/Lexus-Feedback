8d2adca3cbe776a1e6c1f73e1b060f7cd49a5f79
bera5186 <rbasu611@gmail.com>
2019-08-18 19:32:53 +0530

    Done all production settings

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from send_mail import send_mail
 
 app = Flask(__name__)
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:rahul123@localhost/lexus'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rcdbqwswhkwtjv:aab6b283550d3c26076cb8f2f23b2c87a26a26d263e889287882ecf6bc61f155@ec2-75-101-147-226.compute-1.amazonaws.com:5432/dfpr03kcna5j01'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
