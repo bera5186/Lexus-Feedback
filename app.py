fb673e2978b315da634288cccd524646f96ae2c4
bera5186 <rbasu611@gmail.com>
2019-08-18 18:39:35 +0530

    Created database tables and connected frontend to database

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 from flask import Flask, flash, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:rahul123@localhost/lexus'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key = True)
+    customer = db.Column(db.String(200), unique = True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 @app.route('/')
 def index():
@@ -17,8 +44,16 @@ def submit():
         #print(customer, dealer, rating, comments)
         if customer == '' or dealer == '':
             return render_template('index.html', message="Please enter required feilds")
-        return render_template('sucess.html')
+        
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('sucess.html')
+        return render_template('index.html', message="You have already submitted a feedback")
+
+        
 
 if __name__ == '__main__':
-    app.debug = True
+    
     app.run()
